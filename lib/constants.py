7b27c76e18100774ff94d61d24d69df100d39471
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-21 18:48:58 +0200

    Reset styles in colors

--- lib/constants.py
+++ lib/constants.py
@@ -7,10 +7,10 @@ import colorama
 IS_EFI = pathlib.Path("/sys/firmware/efi/efivars").exists()
 DEBUG = os.getenv("DEBUG", False)
 
-SUCCESS_COLOR = colorama.Fore.GREEN
+SUCCESS_COLOR = colorama.Style.NORMAL + colorama.Fore.GREEN
 ERROR_COLOR = colorama.Fore.RED + colorama.Style.BRIGHT
-WARN_COLOR = colorama.Fore.YELLOW
-INFO_COLOR = colorama.Fore.CYAN
-NOTE_COLOR = colorama.Fore.RESET + colorama.Style.DIM
-DEBUG_COLOR = colorama.Fore.BLUE
-CMD_COLOR = colorama.Fore.MAGENTA
+WARN_COLOR = colorama.Style.NORMAL + colorama.Fore.YELLOW
+INFO_COLOR = colorama.Style.NORMAL + colorama.Fore.CYAN
+NOTE_COLOR = colorama.Style.DIM + colorama.Fore.RESET
+DEBUG_COLOR = colorama.Style.NORMAL + colorama.Fore.BLUE
+CMD_COLOR = colorama.Style.NORMAL + colorama.Fore.MAGENTA
