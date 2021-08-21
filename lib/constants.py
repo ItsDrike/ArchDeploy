c8851cf0351917c14df84a673ce2ae7a157f6926
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-21 19:58:32 +0200

    Prefix all colors with reset color

--- lib/constants.py
+++ lib/constants.py
@@ -7,11 +7,12 @@ import colorama
 IS_EFI = pathlib.Path("/sys/firmware/efi/efivars").exists()
 DEBUG = os.getenv("DEBUG", False)
 
-SUCCESS_COLOR = colorama.Style.NORMAL + colorama.Fore.GREEN
-ERROR_COLOR = colorama.Fore.RED + colorama.Style.BRIGHT
-WARN_COLOR = colorama.Style.NORMAL + colorama.Fore.YELLOW
-INFO_COLOR = colorama.Style.NORMAL + colorama.Fore.CYAN
-NOTE_COLOR = colorama.Style.DIM + colorama.Fore.RESET
-DEBUG_COLOR = colorama.Style.NORMAL + colorama.Fore.BLUE
-CMD_COLOR = colorama.Style.NORMAL + colorama.Fore.MAGENTA
+
 RESET_COLOR = colorama.Style.RESET_ALL + colorama.Fore.RESET
+SUCCESS_COLOR = RESET_COLOR + colorama.Fore.GREEN
+ERROR_COLOR = RESET_COLOR + colorama.Fore.RED + colorama.Style.BRIGHT
+WARN_COLOR = RESET_COLOR + colorama.Fore.YELLOW
+INFO_COLOR = RESET_COLOR + colorama.Fore.CYAN
+NOTE_COLOR = RESET_COLOR + colorama.Style.DIM
+DEBUG_COLOR = RESET_COLOR + colorama.Fore.BLUE
+CMD_COLOR = RESET_COLOR + colorama.Fore.MAGENTA
