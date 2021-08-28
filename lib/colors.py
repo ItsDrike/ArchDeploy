5e7e6a9c9793e8827b16a14009301837bb1676fb
ItsDrike <itsdrikeofficial@gmail.com>
2021-08-28 19:20:42 +0200

    Fix ANSI color ordering

--- lib/colors.py
+++ lib/colors.py
@@ -6,11 +6,10 @@ class ANSIColor:
     BLACK = "\033[0;30m"
     RED = "\033[0;31m"
     GREEN = "\033[0;32,"
-    YELLOW = "\033[1;33m"
+    BROWN = "\033[0;33m"
     BLUE = "\033[0;34m"
     MAGENTA = "\033[0;35m"
     CYAN = "\033[0;36m"
-
     LIGHT_GRAY = "\033[0;37m"
     DARK_GRAY = "\033[1;30m"
     LIGHT_RED = "\033[1;31m"
